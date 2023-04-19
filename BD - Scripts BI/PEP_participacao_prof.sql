dd6b89dad115b7d58a63d59b737b404dfcc56609
M4athss <matheus-augustob@hotmail.com>
2023-04-19 16:19:31 -0300

    Profissional

---
+++ BD - Scripts BI/PEP_participacao_prof.sql
@@ -0,0 +1,22 @@
+select
+    a.cod_atendimento as registro,
+    d.respalfa as conselho,
+    f.descricao as grau_participacao
+
+from
+    atcabecatend a
+    inner join htatendimento b on b.id_atcabecatend = a.id
+    inner join prcabevol c on c.id_htatendimento = b.id
+    inner join prevolpac d on d.id_cabevol = c.id_cabevol
+    inner join prresposta e on e.id_resposta = d.id_resposta
+    inner join prpergunta f on e.id_prpergunta = f.id
+    inner join prevolucao g on g.id_evolucao = c.id_evolucao
+
+where
+    a.cod_atendimento = 39216
+    and g.cod = 'E15'
+    and f.cod_pergunta in ('2752','2753','2754','2755','2756','2757')
+    and a.tp_atendimento = 'I'
+    and c.id_tbcbopro_suspensao is null
+order by
+    c.data_hora_evolucao desc
