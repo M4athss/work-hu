36c7c826cde375eef7d75c86ce38aa225b7b5e51
M4athss <matheus-augustob@hotmail.com>
2023-04-19 16:51:01 -0300

    PEP

---
+++ BD - Scripts BI/PEP_cid_internacao.sql
@@ -0,0 +1,22 @@
+select
+    a.cod_atendimento,
+    i.cod
+from
+    atcabecatend a
+    inner join htatendimento b on b.id_atcabecatend = a.id
+    inner join prcabevol c on c.id_htatendimento = b.id
+    inner join prevolpac d on d.id_cabevol = c.id_cabevol
+    inner join prresposta e on e.id_resposta = d.id_resposta
+    inner join prpergunta f on e.id_prpergunta = f.id
+    inner join prevolucao g on g.id_evolucao = c.id_evolucao
+    inner join htcid h on h.id = d.id_htcid
+    inner join tbcid10 i on i.id = h.id_tbcid10
+where
+    a.cod_atendimento = 39241 --Registro do atendimento
+    and g.cod in ('M06','E09') --Templates Evolução médica e sumário de alta.
+    and f.tipoperg = 10 --Separa o tipo CID de pergunta nos formularios
+    and a.tp_atendimento = 'I' --tipo de atendimento somente interncao
+    and c.id_tbcbopro_suspensao is null --IS NULL indica que a evolução não foi suspensa, caso contrário teria o CBO do profissional resposável pela suspensão.
+order by
+    c.data_hora_evolucao desc
+limit 1
