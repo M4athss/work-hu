9112cdfbbd0792d75784329e4b32bb755339ce39
M4athss <matheus-augustob@hotmail.com>
2023-05-23 16:43:28 -0300

    Backup

--- BD/PEP/snd.sql
+++ BD/PEP/snd.sql
@@ -2,13 +2,16 @@ select
     a.leito,
     a.segurado as nome,
     b.nasc as data_nascimento,
-    datediff(year, b.nasc, current_date) as idade,
-    cast(a.entrada as date) as entrada,
-    substr(a.horaent,1,2) || ':' || substr(a.horaent,3,4) as entrada_hora,
-    datediff(day, a.entrada, current_date) as Dias_internado
+    substr(((datediff(day, b.nasc, current_date))/365.25),1,2) as idade,
+    c.data_hora_entrada,
+    (datediff(hour, c.data_hora_entrada, current_date)/24) as Dias_internado
 from
     ricadint a
     inner join ricadpac b on b.pront = a.pront
+    inner join atcabecatend c on c.cod_atendimento = a.reg
 where
     a.alta = '30.12.1899'
-    and datediff(day, a.entrada, current_date) >= 2
+    and c.tp_atendimento = 'I'
+    and (datediff(hour, c.data_hora_entrada, current_date)/24) >= 2
+order by
+    c.data_hora_entrada
